dfe8bc160759b2bf05727e7c5a25367c7a612a31
Alejandro Figueroa <afigueroa@arkusnexus.com>
2014-05-21 16:06:25 -0700

    New code for 2 buttons

--- cancrusher.ino
+++ cancrusher.ino
@@ -1,14 +1,15 @@
-int pin_button = 13;
-int pin_return = 7;
-int pin_relay1 = 12;
-int pin_relay2 = 11;
+int pin_button_open = 13;
+int pin_button_close = 12;
+int pin_relay1 = 11;
+int pin_relay2 = 10;
+int foto = 0;
 int lastValue = 0;
 
 void setup()
 {
 	Serial.begin(9600);
-	pinMode(pin_button, INPUT);
-	pinMode(pin_return, INPUT);
+	pinMode(pin_button_open, INPUT);
+	pinMode(pin_button_close, INPUT);
 	pinMode(pin_relay1, OUTPUT);
 	pinMode(pin_relay2, OUTPUT);
 	digitalWrite(pin_relay1, LOW);
@@ -17,20 +18,24 @@ void setup()
 
 void loop()
 {
-	if(digitalRead(pin_button) == HIGH && lastValue == 0)
+
+	Serial.println(analogRead(foto));
+
+	if(digitalRead(pin_button_open) == LOW && lastValue == 0)
 	{
 		Serial.println("OPEN");
 		lastValue = 1;
-		digitalWrite(pin_relay1, HIGH);
-		digitalWrite(pin_relay2, LOW);
+		digitalWrite(pin_relay1, LOW);
+		digitalWrite(pin_relay2, HIGH);
 		delay(300);
 	}
-	else if(digitalRead(pin_return) == HIGH)
+	else if(digitalRead(pin_button_close) == LOW)
 	{
 		Serial.println("CLOSE");
 		lastValue = 0;
-		digitalWrite(pin_relay1, LOW);
-		digitalWrite(pin_relay2, HIGH);
+		digitalWrite(pin_relay1, HIGH);
+		digitalWrite(pin_relay2, LOW);
+		delay(300);
 	}
 	else{
 		Serial.println("NOTHING");
